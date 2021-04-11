38e235c005c92d3ea0af3e5b918653694d24f9a6
EJM <erin.jay.moyer@gmail.com>
2021-04-10 22:14:28 -0600

    Added Initial css styles. Created base template for how page will look.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {makeDailyItem, toggleDailyComplete, toggleDailyPriority, changeDailyDeta
 import {addDaily, deleteDaily, displayDailyProjects} from './dailyprojects.js'
 import {makeProjectItem, toggleComplete, togglePriority, changeDetail, changeName} from './projectitem.js';
 import {getCurrentProjectName, makeProject, toggleProject, addProjectItem, deleteProject, displayProjects, displayCurrentProject, deleteProjectItem} from './projects.js'
-
+import './screencontent.js';
 //Project and dailyProject functionally now good to go 
 const Japanese = makeProjectItem("Japanese", "Learn all the Japanese", "High");
 const Spanish = makeProjectItem("Spanish", "learn all the spanish", "Low" );
